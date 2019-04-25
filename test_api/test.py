d7aa393ca300f306d7d2e6c1e111c548dfb88d66
Rusty <rust.roberts@protonmail.com>
2019-04-24 21:02:39 -0500

    testing yahoo api

---
+++ test_api/test.py
@@ -0,0 +1,17 @@
+from rauth import OAuth1Service
+import json
+
+def new_decoder(payload):
+    return json.loads(payload.decode('utf-8'))
+
+yahoo = OAuth1Service(
+    name='yahoo',
+    consumer_key='dj0yJmk9bXpycjhkTjJTdFhwJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWZk',
+    consumer_secret='0ae6007f4a053cf2dc061b4fae8d4d202158ce22',
+    request_token_url='https://api.login.yahoo.com/oauth/v2/get_request_token',
+    authorize_url='https://api.login.yahoo.com/oauth/v2/request_auth',
+    access_token_url='https://api.login.yahoo.com/oauth/v2/get_token',
+    base_url='http://fantasysports.yahooapis.com/'
+)
+
+request_token = yahoo.get_request_token()
