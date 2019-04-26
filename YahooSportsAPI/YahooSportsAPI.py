2f7e2e2558e7af96cfca39759a9a996138898844
Marcus <marcusabakke@gmail.com>
2019-04-25 21:54:04 -0400

    Updated YahooSportsAPI with comments

--- YahooSportsAPI/YahooSportsAPI.py
+++ YahooSportsAPI/YahooSportsAPI.py
@@ -4,6 +4,19 @@
 # Description:  This file defines a class variable which is used 
 #               to initialize Yahoo Sports API authorization.
 #
+# oauth.json:   User must create an app via the Yahoo Developer
+#               Network here:  https://developer.yahoo.com/apps/
+#               Once the app is created, the user must create
+#               an oauth.json file with the following format:
+#
+#               {
+#                   "consumer_key": "Your Key Here", 
+#                   "consumer_secret": "Your Secret Here", 
+#               }
+#
+#               This oauth.json file should be within a folder
+#               named Authorization in the root directory.
+#
 from yahoo_oauth import OAuth2
 
 class YahooSportsAPI:
