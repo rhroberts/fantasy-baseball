110955089c3f1681dcd65d6952d7f4126f09ccef
Marcus <marcusabakke@gmail.com>
2019-05-01 22:55:06 -0400

    Modified get_league_info, get_league_team, and get_league_matchup to return structured dataframe

--- hometown_homies/yahoo_api.py
+++ hometown_homies/yahoo_api.py
@@ -1,9 +1,11 @@
 from yahoo_oauth import OAuth2
 from pathlib import Path
 import pandas as pd
+# import ipdb
 
 
-LEAGUE_URL = 'https://fantasysports.yahooapis.com/fantasy/v2/league/mlb.l.69542'
+LEAGUE_URL = 'https://fantasysports.yahooapis.com/fantasy/v2/'
+LEAGUE_ID = 'mlb.l.69542'
 
 def get_session(auth_file=None):
     """Get a valid yahoo session object"""
@@ -20,27 +22,143 @@ def get_session(auth_file=None):
 
 def get_league_info(session):
     """Get league info from a Yahoo session object"""
-    league_info = session.get(LEAGUE_URL, params={'format': 'json'})
     
-    return league_info
+    # Get info from API
+    league_info = session.get(LEAGUE_URL + 'league/' + LEAGUE_ID, params={'format': 'json'})
+    
+    # Reformat info as dict
+    league_dict = league_info.json()['fantasy_content']['league'][0]
+    
+    # Get dict keys
+    k = list(league_dict.keys())
+    
+    # Preallocate dict and assign variables from keys
+    complete_dict = {}
+    complete_dict['Variable'] = k
+    
+    # Loop through dict and save results
+    for i in range(0,len(league_dict)):
+        complete_dict.setdefault('Value', []).append(league_dict[k[i]])
+    
+    # Combine dictionary into dataframe
+    league_df = pd.DataFrame(complete_dict,columns=['Value','Variable'])
+    league_df = league_df.set_index('Variable')
+    
+    return league_df
 
 def get_league_standings(session):
     """Get leage standings from a Yahoo session object"""
-    standings_info = session.get(LEAGUE_URL + '/standings', params={'format': 'json'})
+    standings_info = session.get(LEAGUE_URL + 'league/' + LEAGUE_ID + '/standings', params={'format': 'json'})
     
     return standings_info
 
-def get_league_matchup(session):
+def get_league_matchup(session,team_key,weeks):
     """Get leage matchup from a Yahoo session object"""
-    matchup_info = session.get(LEAGUE_URL + '/matchup', params={'format': 'json'})
     
-    return matchup_info
+    # Get URL with team_key and weeks
+    url = LEAGUE_URL + 'team/' + LEAGUE_ID + team_key[-4:]+'/matchups;weeks='
+    if len(weeks) > 1:
+        for i in range(0,len(weeks)-1):
+            url = url+str(weeks[i])+','
+    url = url+str(weeks[-1])
+    
+    # Get info from API
+    matchup_info = session.get(url, params={'format': 'json'})
+    
+    # Reformat info as dict
+    matchup_dict = matchup_info.json()
 
-def get_league_team(session):
-    """Get leage matchup from a Yahoo session object"""
-    team_info = session.get(LEAGUE_URL + '/teams', params={'format': 'json'})
+    # Define stat key
+    stat_key = ['H/AB*','R','HR','RBI','SB','AVG','IP*','W','SV','K','ERA','WHIP','Points']
     
-    # Get info as dict
+    # Loop through each weeks
+    matchup_df = []
+    for i in range(0,len(weeks)):
+
+        week_i_dict = matchup_dict['fantasy_content']['team'][1]['matchups'][str(i)]['matchup']
+                
+        # Parse team 1 information
+        team_1_dict = {}
+        team_1_info  = week_i_dict['0']['teams']['0']['team'][0]
+        team_1_info = [x for x in team_1_info if x != []]
+        team_1_info = {k: v for d in team_1_info for k, v in d.items()}
+        team_1_name = team_1_info['name']
+        team_1_dict['Team'] = team_1_name
+        team_1_results = week_i_dict['0']['teams']['0']['team'][1]
+        team_1_dict['Points'] = float(team_1_results['team_points']['total'])
+        team_1_stats = team_1_results['team_stats']['stats']
+        for j in range(0,len(stat_key)-1):
+            stat_i = team_1_stats[j]['stat']['value']
+            stat_name = stat_key[j]
+            team_1_dict[stat_name] = stat_i
+        
+        # Parse team 2 information
+        team_2_dict = {}
+        team_2_info  = week_i_dict['0']['teams']['1']['team'][0]
+        team_2_info = [x for x in team_2_info if x != []]
+        team_2_info = {k: v for d in team_2_info for k, v in d.items()}
+        team_2_name = team_2_info['name']
+        team_2_dict['Team'] = team_2_name
+        team_2_results = week_i_dict['0']['teams']['1']['team'][1]
+        team_2_dict['Points'] = float(team_2_results['team_points']['total'])
+        team_2_stats = team_2_results['team_stats']['stats']
+        stats = []
+        for j in range(0,len(stat_key)-1):
+            stat_i = team_2_stats[j]['stat']['value']
+            stat_name = stat_key[j]
+            team_2_dict[stat_name] = stat_i
+        
+        # Combine team dicts
+        complete_dict = {}
+        combine_dict = [team_1_dict,team_2_dict]
+        k = list(team_1_dict.keys())
+        complete_dict['Team'] = [team_1_name,team_2_name]
+        for j in range(0,len(combine_dict)):
+            d = combine_dict[j]
+            for h in range(0,len(stat_key)):
+                if 'H/AB*' in stat_key[h]:
+                    complete_dict.setdefault(stat_key[h], []).append(d[stat_key[h]])
+                else:
+                    complete_dict.setdefault(stat_key[h], []).append(float(d[stat_key[h]]))
+        
+        # Combine dictionary into dataframe
+        tmp = stat_key + ['Team']
+        week_i_df = pd.DataFrame(complete_dict,columns=tmp)
+        week_i_df = week_i_df.set_index('Team')
+        
+        # Append dataframe
+        matchup_df.append(week_i_df)
+
+    return matchup_df
+
+def get_league_team(*arg):
+    """Get basic team information from a Yahoo session object
+    
+    1. First argument is the sessions object for get_session().
+    2. Second argument is a list of indices (0-7) to specify which teams to pull data for.
+        0 = Rusty
+        1 = Curtis
+        2 = Tjos
+        3 = Luke
+        4 = Marcus
+        5 = Peter
+        6 = Cody W
+        7 = Cody H
+    3. If there is no second argument, returns a dataframe for all teams in the league.
+    
+    """
+    
+    # Split arguments
+    if len(arg) > 1:
+        teams = arg[1]
+    else:
+        teams = range(0,8)
+    session = arg[0]
+    
+    # Get info from API
+    team_info = session.get(LEAGUE_URL + 'league/' + LEAGUE_ID + '/teams', params={'format': 'json'})
+    
+    # Reformat info as dict
     team_dict = team_info.json()
     
     # Preallocate variables
@@ -48,10 +166,13 @@ def get_league_team(session):
     tmp_dict = []
     
     # Name string associated with each team
-    names = ['Rusty','Curtis','Tjos','Luke','Marcus','Peter','Cody W','Cody H']
+    all_names = ['Rusty','Curtis','Tjos','Luke','Marcus','Peter','Cody W','Cody H']
+    
+    # Get reduced set of names
+    names = [all_names[i] for i in teams]
     
     # Loop through each team in the dictionary
-    for i in range(0,len(names)):
+    for i in teams:
         team  = team_dict['fantasy_content']['league'][1]['teams'][str(i)]['team'][0]
         team = [x for x in team if x != []]
         team = {k: v for d in team for k, v in d.items()}
--- scripts/test_api.py
+++ scripts/test_api.py
@@ -7,15 +7,28 @@ from pandas.io.json import json_normalize
 
 sess = yahoo_api.get_session()
 
+# Example usage for get_league_info()
 league_info = yahoo_api.get_league_info(sess)
-league_name = league_info.json()['fantasy_content']['league'][0]['name']
+league_name = league_info.loc['name'].values[0]
 print(f"Our league name is: {league_name}!!!")
 
 standings_info = yahoo_api.get_league_standings(sess)
 standings_dict = standings_info.json()
 print("We got the standings but it's a big-ass dict.")
 
-matchup_info = yahoo_api.get_league_matchup(sess)
-matchup_dict = matchup_info.json()
+# Example usage for get_league_team()
+team_info  = yahoo_api.get_league_team(sess,[0,1,2,3])  # Get team info for Rusty, Curtis, Tjos, and Luke
+team_dmarc = yahoo_api.get_league_team(sess,[4])        # Get team info for Marcus only
+team_info2 = yahoo_api.get_league_team(sess)            # Get team info for everyone
 
-team_info = yahoo_api.get_league_team(sess)
+# Example usage for get_league_matchup()
+team_key = team_dmarc.loc['team_key','Marcus']
+weeks = [1,2,3,4,5]
+matchup_info = yahoo_api.get_league_matchup(sess,team_key,weeks)
+
+# Temporary print to visualize matchup_info
+print()
+for i in range(0,len(matchup_info)):
+    print(matchup_info[i])
+    print()
+    print()
