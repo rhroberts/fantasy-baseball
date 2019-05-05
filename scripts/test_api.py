4e592d92ef00dcedf00d6cfe5eb96cb837db2bf3
Marcus <marcusabakke@gmail.com>
2019-05-04 20:06:39 -0400

    Added matchup_stats.py script. Small edit to match_team_keys in yahoo_api.py

--- scripts/test_api.py
+++ scripts/test_api.py
@@ -3,6 +3,7 @@
 from hometown_homies import yahoo_api
 import pandas as pd
 from pandas.io.json import json_normalize
+import ipdb
 
 sess = yahoo_api.get_session()
 
@@ -18,12 +19,12 @@ print(standings_df)
 print()
 
 # Example usage for get_league_team()
-team_info  = yahoo_api.get_team_info(sess,[0,1,2,3])  # Get team info for Rusty, Curtis, Tjos, and Luke
-team_dmarc = yahoo_api.get_team_info(sess,[4])        # Get team info for Marcus only
+team_info  = yahoo_api.get_team_info(sess,[1,2,3,4])  # Get team info for Rusty, Curtis, Tjos, and Luke
+team_dmarc = yahoo_api.get_team_info(sess,[5])        # Get team info for Marcus only
 team_info2 = yahoo_api.get_team_info(sess)            # Get team info for everyone
 
 # Example usage for get_league_matchup()
-team_key = team_dmarc.loc['team_key','Marcus']
+team_key = team_dmarc.loc['team_key',team_dmarc.columns.values[0]]
 weeks = [1,2,3,4,5]
 matchup_info = yahoo_api.get_league_matchup(sess,team_key,weeks)
 
@@ -33,50 +34,50 @@ for i in range(0,len(matchup_info)):
     print()
 
 # Example usage for calculating average weekly statistics
-# 0 = Rusty
-# 1 = Curtis
-# 2 = Tjos
-# 3 = Luke
-# 4 = Marcus
-# 5 = Peter
-# 6 = Cody W
-# 7 = Cody H
-average_df = yahoo_api.get_team_averages(sess,0)
+# 1 = Lone Star Tallboys    = Rusty
+# 2 = Curt's American Made  = Curtis
+# 3 = Death By Smalls       = Tjos
+# 4 = Luke's Legit Team     = Luke
+# 5 = Hello World           = Marcus
+# 6 = Mookie and the Betts  = Peter
+# 7 = Soggy Dingers         = Cody W
+# 8 = Chuck Nazty           = Cody H
+average_df = yahoo_api.get_team_averages(sess,1)
 print("Statistical Summary for Rusty's Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,1)
+average_df = yahoo_api.get_team_averages(sess,'American ')
 print("Statistical Summary for Curtis' Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,2)
+average_df = yahoo_api.get_team_averages(sess,3)
 print("Statistical Summary for Tjos' Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,3)
+average_df = yahoo_api.get_team_averages(sess,'Legit')
 print("Statistical Summary for Luke's Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,4)
+average_df = yahoo_api.get_team_averages(sess,'Hello World')
 print("Statistical Summary for Marcus' Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,5)
+average_df = yahoo_api.get_team_averages(sess,6)
 print("Statistical Summary for Peter's Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,6)
+average_df = yahoo_api.get_team_averages(sess,7)
 print("Statistical Summary for Cody W's Team")
 print(average_df)
 print()
 
-average_df = yahoo_api.get_team_averages(sess,7)
+average_df = yahoo_api.get_team_averages(sess,'Nazty')
 print("Statistical Summary for Cody H's Team")
 print(average_df)
 print()
